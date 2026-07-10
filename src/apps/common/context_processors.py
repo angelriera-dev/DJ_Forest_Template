d125cd1c28a7b3c250a977ec49a9086fb18e72e2
angelr <angelriera.dev@gmail.com>
2026-07-10 16:51:10 +0000

    lint: common.context_proressors

--- src/apps/common/context_processors.py
+++ src/apps/common/context_processors.py
@@ -1,15 +1,24 @@
-
-
 from typing import TYPE_CHECKING
+
 if TYPE_CHECKING:
     import django.core.handlers.wsgi
 NAV_LINKS = [
     {"name": "Dashboard", "url": "dashboard:home", "icon": "🏠"},
-    { "name": "Edit profile", "url": "dashboard:profile", "icon": "👤", },
-    { "name": "Settings", "url": "dashboard:settings", "icon": "⚙️", },
-    { "name": "Upgrade plan", "url": "dashboard:subscription_plans", "icon": "💳" },
-
-
+    {
+        "name": "Edit profile",
+        "url": "dashboard:profile",
+        "icon": "👤",
+    },
+    {
+        "name": "Settings",
+        "url": "dashboard:settings",
+        "icon": "⚙️",
+    },
+    {"name": "Upgrade plan", "url": "dashboard:subscription_plans", "icon": "💳"},
 ]
-def navigation(request: "django.core.handlers.wsgi.WSGIRequest") -> dict[str, list[dict[str, str]]]:
-    return {'nav_items': NAV_LINKS}
+
+
+def navigation(
+    request: "django.core.handlers.wsgi.WSGIRequest",
+) -> dict[str, list[dict[str, str]]]:
+    return {"nav_items": NAV_LINKS}
